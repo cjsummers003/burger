1d5c18c818535ac1a44555486b3b8ec65c562623
cj summers <cjsummers003@hotmail.com>
2019-03-06 11:07:17 -0500

    db update

--- config/connection.js
+++ config/connection.js
@@ -13,13 +13,13 @@ connection= mysql.createConnection(process.env.JAWDB_URL);
   });
 };
 
-var connection = mysql.createConnection({
-  port: 3306,
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "burgers_db"
-});
+// var connection = mysql.createConnection({
+//   port: 3306,
+//   host: "localhost",
+//   user: "root",
+//   password: "",
+//   database: "burgers_db"
+// });
 
 // Make connection.
 connection.connect(function(err) {
